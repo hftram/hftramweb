fdee0f8b3ebd693dbe00f3601d6f6050ef31c3cd
hftram <hftram@gmail.com>
2016-06-10 00:41:22 -0400

    commit4

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/james/Dropbox/HF2016summer/HF2016REU_SHARE/WebsiteProject/hftramweb/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
