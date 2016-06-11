eea2abf9a6913ca2eb9d398e05fde6f1b7f5f2a9
hftram <hftram@gmail.com>
2016-06-10 23:27:04 -0400

    commit6

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,8 +88,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = "/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
